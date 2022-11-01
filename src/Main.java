a86cddd29cd1ca880709f79acf9ad715ec6ead32
Irinaabieva <irinaabieva03@gmail.com>
2022-11-01 18:30:32 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         // Задание 1
-        int clientOS = 0;
+        int clientOS = 1;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
         if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        // Задание 2
+        int clientOs = 0;
+        int clientDeviceYear = 2013;
+        if (clientOs == 0) {
+            if (clientDeviceYear > 2014) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        }   else {
+                if (clientDeviceYear > 2014) {
+            System.out.println("Установите  версию приложения для Android по ссылке");
+                } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
         }
     }
 }
