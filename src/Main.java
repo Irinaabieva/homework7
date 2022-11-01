165f6d9c417ee9ced4461164530fd9dada488498
Irinaabieva <irinaabieva03@gmail.com>
2022-11-01 19:09:26 +0300

    Задание 4

--- src/Main.java
+++ src/Main.java
@@ -32,5 +32,17 @@ public class Main {
         } else {
             System.out.println("Год явлется весокосным");
         }
+
+        // Задание 4
+        int deliveryDistance = 95;
+        int deliveryDays = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: " + deliveryDays );
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется дней: " + (deliveryDays + 1));
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: " + (deliveryDays + 2));
+        }
+
     }
 }
