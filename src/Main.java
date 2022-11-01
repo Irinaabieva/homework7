3a6a35db24a466c1b494ec4b4698402eea83960d
Irinaabieva <irinaabieva03@gmail.com>
2022-11-01 19:17:38 +0300

    Задание 5

--- src/Main.java
+++ src/Main.java
@@ -43,6 +43,31 @@ public class Main {
         } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
             System.out.println("Потребуется дней: " + (deliveryDays + 2));
         }
-
+        // Задание 5
+        int monthNumber = 13;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+            System.out.println("Месяцев в году не может быть больше 12ти");
+        }
     }
 }
